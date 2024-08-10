5db5ecff547b362279ada4cfa35e4608a1f187ce
ceoDemitri <demichole@gmail.com>
2024-08-09 22:00:27 -0500

    rename app

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f04ce3514187d6dffb6896faaddcbd0d9be7cbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94061c72c1af03e46bb0cbda3a52ae7c5c972ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
