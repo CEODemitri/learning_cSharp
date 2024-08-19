769d7f5f82de0d6a59d01f56b99dde197507d76a
ceoDemitri <demichole@gmail.com>
2024-08-19 16:47:29 -0500

    slow motion i tell you

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00ca5f10895315d04ae469efc9ec1e81570b04e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a87517fb54441c7f78efd9e1b6c1f11354ce99c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
