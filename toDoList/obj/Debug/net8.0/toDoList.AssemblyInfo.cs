0b84570c929693365ac5476ccc9bfb6ada3baf30
ceoDemitri <demichole@gmail.com>
2024-10-18 09:57:13 -0500

    slight update, play around writing a bit of csharp for realtime view of typed ch on to do app page

--- toDoList/obj/Debug/net8.0/toDoList.AssemblyInfo.cs
+++ toDoList/obj/Debug/net8.0/toDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("toDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3110c49a783f9bc213d805394a3615fe86511e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0b73d0d4e0c86fd1ebdba6166abca2d197401a")]
 [assembly: System.Reflection.AssemblyProductAttribute("toDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("toDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
