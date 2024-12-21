36045c2b7d35ec612fa9e2813e1e2ddf80404b6c
ceoDemitri <demichole@gmail.com>
2024-12-21 12:51:55 -0600

    garbage collection explanation, demonstration and test

---
+++ essential/GarbageCollect/Program.cs
@@ -0,0 +1,23 @@
+﻿void BigOperation()
+{
+    byte[] myArray = new byte[1000000];
+
+    Console.WriteLine($"Allocated memory is : {GC.GetTotalMemory(false)}");
+    Console.ReadLine();
+}
+
+// Retrieve and print total memory allocated
+Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
+Console.ReadLine();
+
+// Call the function that allocated large memory chunk
+BigOperation();
+
+// Force a garbage collection
+GC.Collect();
+// will probably never need to call this function. demonstration and testing purposes
+
+// Retrieve and print total memory allocated
+Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
+Console.ReadLine();
+
